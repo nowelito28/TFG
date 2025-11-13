e67cdc5d3a0a11b790e0d3c33489750e74167f4f
nowelito28 <noelrp240514@gmail.com>
2025-11-13 19:48:47 +0100

    kernel style LKM

--- LKM/crypto_fd.c
+++ LKM/crypto_fd.c
@@ -29,6 +29,7 @@
 // unsigned char K[]; unsigned int K_len=64;
 #include "k_embedded.h"
 
+
 enum {
 	BUFSIZE = 100,
 	MAX_PROC_SIZE = 20480,
@@ -36,34 +37,39 @@ enum {
 	PID_SIZE = 11,
 	GID_SIZE = 11,
 	CMD_SIZE = 50,
-	PS_LINE_SIZE =
-	   UID_SIZE + PID_SIZE + + GID_SIZE + CMD_SIZE,
+	PS_LINE_SIZE = UID_SIZE + PID_SIZE + +GID_SIZE + CMD_SIZE,
 };
+
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Noel");
 
-// Puntero de referencia/entrada al fichero que crearemos en /proc -->
-// /proc/fddev
+
+// Referencia a fichero en /proc --> /proc/fddev
 static struct proc_dir_entry *ent;
+
 
 // Separador entre el contenido del fichero y el contenido del kernel:
 static const char sep[] = "\n--KERNEL-PS--\n";
 static const int sep_len = sizeof(sep) - 1;	// NO contar '\0'
 
+
 // Separador entre el contenido del kernel y el HMAC en base 64:
 static const char sep_hmac[] = "\n--HMAC--\n";
 static const int sep_hmac_len = sizeof(sep_hmac) - 1;
+
 
 // Cabecera para registro de procesos:
 static const char header[] = "UID        PID        GID        COMMAND\n";
 static const int header_len = sizeof(header) - 1;
 
+
 // Helper --> Escribir todo el contenido que se pase en f en la posición ppos
 // del fichero: Devuelve bytes escritos (off) en éxito <-> <0 en error <-> 0 si
 // encuentra EOF
 static int write_full(struct file *f, const char *buf, int len) {
-	int w, off = 0;
+	int w = 0;
+	int off = 0;
 	loff_t *ppos = &f->f_pos;
 
 	while (off < len) {
@@ -76,6 +82,7 @@ static int write_full(struct file *f, const char *buf, int len) {
 			return -EIO;
 
 		off += w;
+
 	}
 
 	return off;
@@ -89,14 +96,17 @@ static int pad_str_right(char *buf, int curr_len, int buf_len, char pad_char) {
 	if (curr_len >= buf_len) {
 		padding = 0;
 		curr_len = buf_len;
+
 	} else {
 		padding = buf_len - curr_len;
+
 	}
 
 	// Aplicar relleno
 	if (padding > 0) {
 		memset(buf + curr_len, pad_char, padding);
 		curr_len += padding;
+		
 	}
 
 	return buf_len;
@@ -110,11 +120,13 @@ static int safe_chunk(u8 *dst, int *current_len, char *src, int src_len) {
 	if (max_len < src_len) {
 		return -ENOSPC;
 	}
+
 	// Copiar el contenido
 	memcpy(dst + *current_len, src, src_len);
 
 	// Actualizar la longitud escrita
 	*current_len += src_len;
+
 	return src_len;
 }
 
@@ -126,8 +138,10 @@ static int get_uid_str(kuid_t uid_struct, char *uid_str) {
 
 	if (uid == 0) {
 		uid_len = snprintf(uid_str, UID_SIZE, "0/root");
+
 	} else {
 		uid_len = snprintf(uid_str, UID_SIZE, "%d", uid);
+
 	}
 
 	pad_str_right(uid_str, uid_len, UID_SIZE, ' ');
@@ -159,8 +173,10 @@ static int get_gid_str(struct task_struct *task, char *gid_str) {
 
 	if (gid == 0) {
 		gid_len = snprintf(gid_str, GID_SIZE, "0/root");
+
 	} else {
 		gid_len = snprintf(gid_str, GID_SIZE, "%d", gid);
+
 	}
 
 	pad_str_right(gid_str, gid_len, GID_SIZE, ' ');
@@ -176,23 +192,29 @@ static int get_command_str(struct task_struct *task, char *comm_str) {
 	// Proceo sin espacio en memoria -> kernel thread:
 	if (task->mm == NULL) {
 		comm_len = snprintf(comm_str, CMD_SIZE, "[%s]\n", task->comm);
+
 	} else {
 		// Pocesos user -> ruta completa
 		struct file *exe_file = task->mm->exe_file;
 
 		if (exe_file) {
-			char *path = d_path(&exe_file->f_path, comm_str, CMD_SIZE);
+			char *path =
+			    d_path(&exe_file->f_path, comm_str, CMD_SIZE);
 
 			if (!IS_ERR(path)) {
-				comm_len = snprintf(comm_str, CMD_SIZE, "%s\n", path);
-				goto cmmd_finished;
+				comm_len =
+				    snprintf(comm_str, CMD_SIZE, "%s\n", path);
+
+				goto commd_finished;
+
 			}
 		}
 
 		comm_len = snprintf(comm_str, CMD_SIZE, "%s\n", task->comm);
+
 	}
 
-      cmmd_finished:
+      commd_finished:
 	if (comm_len >= CMD_SIZE)
 		comm_len = CMD_SIZE - 1;
 
@@ -209,21 +231,25 @@ static int write_cont_hmac(struct file *f, const char *cont, int cont_len,
 	w = write_full(f, sep, sep_len);
 	if (w < 0)
 		return w;
+
 	total += w;
 
 	w = write_full(f, cont, cont_len);
 	if (w < 0)
 		return w;
+
 	total += w;
 
 	w = write_full(f, sep_hmac, sep_hmac_len);
 	if (w < 0)
 		return w;
+
 	total += w;
 
 	w = write_full(f, hmac_b64, hmac_b64len);
 	if (w < 0)
 		return w;
+
 	total += w;
 
 	return total;
@@ -234,8 +260,7 @@ static int write_cont_hmac(struct file *f, const char *cont, int cont_len,
 static int get_hmac_sha256(const u8 *buf, int buf_len, u8 **hmac, int *hmac_len) {
 	int rv = 0;
 
-	// Handler del Crypto API del kernel para un hash síncrono --> shash
-	// Transformador:
+	// Handler Crypto API del kernel -> hash síncrono --> shash (transformador)
 	struct crypto_shash *tfm;
 
 	// 1) HAMC(SHA-256) como algoritmo
@@ -252,10 +277,13 @@ static int get_hmac_sha256(const u8 *buf, int buf_len, u8 **hmac, int *hmac_len)
 
 	// 3) Reservar memoria para el HMAC (calculando su tamaño)
 	*hmac_len = crypto_shash_digestsize(tfm);
+
 	*hmac = kmalloc(*hmac_len, GFP_KERNEL);
 	if (!*hmac) {
 		rv = -ENOMEM;
+
 		goto out_free_tfm;
+
 	}
 
 	// 4) Calcular el HMAC en una sola llamada (one-shot):
@@ -274,6 +302,7 @@ static int get_hmac_sha256(const u8 *buf, int buf_len, u8 **hmac, int *hmac_len)
 
       out_free_tfm:
 	crypto_free_shash(tfm);
+
 	return rv;
 }
 
@@ -286,6 +315,7 @@ static int get_hmac_b64(const u8 *hmac, int hmac_len, u8 **hmac_b64,
 
 	*hmac_b64 = kmalloc(hmac_b64cap, GFP_KERNEL);
 	if (!*hmac_b64)
+
 		return -ENOMEM;
 
 	// 2) Codificar el HMAC a Base64 y guardar la longitud real escrita en
@@ -293,7 +323,9 @@ static int get_hmac_b64(const u8 *hmac, int hmac_len, u8 **hmac_b64,
 	*hmac_b64len = base64_encode(hmac, hmac_len, *hmac_b64);
 	if (*hmac_b64len < 0) {
 		kfree(*hmac_b64);
+
 		return *hmac_b64len;
+
 	}
 
 	return 0;
@@ -311,6 +343,7 @@ static int ps_data(struct task_struct *task, u8 *cont, int *cont_len) {
 	int uid_len = get_uid_str(task_uid(task), uid_str);
 	if (uid_len <= 0)
 		goto out_fail;
+
 	if (safe_chunk(cont, cont_len, uid_str, UID_SIZE) < 0)
 		goto out_fail;
 
@@ -318,6 +351,7 @@ static int ps_data(struct task_struct *task, u8 *cont, int *cont_len) {
 	int pid_len = get_pid_str(task_pid_nr(task), pid_str);
 	if (pid_len <= 0)
 		goto out_fail;
+
 	if (safe_chunk(cont, cont_len, pid_str, PID_SIZE) < 0)
 		goto out_fail;
 
@@ -325,6 +359,7 @@ static int ps_data(struct task_struct *task, u8 *cont, int *cont_len) {
 	int gid_len = get_gid_str(task, gid_str);
 	if (gid_len <= 0)
 		goto out_fail;
+
 	if (safe_chunk(cont, cont_len, gid_str, GID_SIZE) < 0)
 		goto out_fail;
 
@@ -332,12 +367,14 @@ static int ps_data(struct task_struct *task, u8 *cont, int *cont_len) {
 	int comm_len = get_command_str(task, comm_str);
 	if (comm_len <= 0)
 		goto out_fail;
+
 	if (safe_chunk(cont, cont_len, comm_str, comm_len) < 0)
 		goto out_fail;
 
 	return 0;
 
       out_fail:
+
 	return 1;
 }
 
@@ -347,8 +384,11 @@ static int get_ps(u8 **cont, int *cont_len) {
 	struct task_struct *task;
 
 	*cont = (u8 *) kmalloc(MAX_PROC_SIZE, GFP_KERNEL);
-	if (!*cont)
+	if (!*cont) {
+		printk(KERN_ERR "get_ps: kmalloc failed\n");
+
 		return -ENOMEM;
+	}
 
 	// 1) Copiar la cabecera y actualizar len:
 	if (safe_chunk(*cont, cont_len, (char *)header, header_len) < 0)
@@ -357,26 +397,33 @@ static int get_ps(u8 **cont, int *cont_len) {
 	// 2) Recorrer todos los procesos del sistema
 	// requiere lock de lectura RCU
 	rcu_read_lock();
+
 	for_each_process(task) {
+
 		if (*cont_len >= MAX_PROC_SIZE - PS_LINE_SIZE) {
 			printk(KERN_WARNING
-			       "get_ps: Process buffer full -> Truncating "
-			       "ps output\n");
+			       "get_ps: Buffer full -> Truncating\n");
+
 			break;
+
 		}
 
 		if (ps_data(task, *cont, cont_len)) {
 			printk(KERN_ERR
 			       "get_ps: Extracting process data failed\n");
 			rcu_read_unlock();
+
 			goto out_fail;
+
 		}
+
 	}
+
 	rcu_read_unlock();
 
 	printk(KERN_INFO
-	       "get_ps: Generated ps-like output of %d bytes.\n",
-	       *cont_len);
+	       "get_ps: Generated ps-like output of %d bytes.\n", *cont_len);
+
 	return 0;
 
       out_fail:
@@ -384,6 +431,7 @@ static int get_ps(u8 **cont, int *cont_len) {
 		kfree(*cont);
 	*cont = NULL;
 	*cont_len = 0;
+
 	return -ENOSPC;
 }
 
@@ -406,9 +454,10 @@ static int printh(struct file *f) {
 	// 1) Información de procesos desde el kernel (contenido):
 	rv = get_ps(&cont, &cont_len);
 	if (rv < 0) {
-		printk(KERN_ERR
-		       "Error printH: get_ps_aux failed: %d\n", rv);
+		printk(KERN_ERR "Error printH: get_ps failed: %d\n", rv);
+
 		goto out;
+
 	}
 
 	// 2) Calcular (HMAC(SHA 256)) con clave K):
@@ -416,7 +465,9 @@ static int printh(struct file *f) {
 	if (rv < 0) {
 		printk(KERN_ERR "Error printH: generating HMAC failed: %d\n",
 		       rv);
+
 		goto out;
+
 	}
 
 	// 3) Pasar el HMAC a Base64:
@@ -424,7 +475,9 @@ static int printh(struct file *f) {
 	if (rv < 0) {
 		printk(KERN_ERR "Error printH: parsing HMAC to Base 64: %d\n",
 		       rv);
+
 		goto out_free_hmac;
+
 	}
 
 	// 4) Escribir contenido y HMAC en el fichero fd -> sep_cont + cont +
@@ -433,19 +486,24 @@ static int printh(struct file *f) {
 	rv = write_cont_hmac(f, cont, cont_len, hmac_b64, hmac_b64len);
 	if (rv < 0) {
 		printk(KERN_ERR "Error printH: writing content: %d\n", rv);
+
 		goto out_free_hmacs;
+
 	}
+
 	printk(KERN_INFO
-	       "printH: file has been written with content certificated by "
-	       "HMAC(SHA-256)\n");
+	       "printH: file has been written with HAMC \n");
 
       out_free_hmacs:
 	kfree(hmac_b64);
+
       out_free_hmac:
 	kfree(hmac);
+
       out:
 	if (cont)
 		kfree(cont);
+
 	return rv;
 }
 
@@ -460,14 +518,18 @@ static int val_metadata(struct file *f) {
 	if (!(f->f_mode & FMODE_WRITE) || !(f->f_flags & O_APPEND)) {
 		printk(KERN_ERR "Error printH: fd given must be writable"
 		       " and in append mode (O_WRONLY/O_RDWR | O_APPEND)\n");
+
 		return -EBADF;
+
 	}
 
 	rv = file_permission(f, MAY_WRITE | MAY_APPEND);
 	if (rv < 0) {
 		printk(KERN_ERR
 		       "Error printH: permissions VFS/LSM denied: %d\n", rv);
+
 		return rv;
+
 	}
 
 	return rv;
@@ -479,7 +541,8 @@ static int val_metadata(struct file *f) {
 // 0 EOF
 static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count,
 		       loff_t *ppos) {
-	int fd, rv = 0;
+	int fd = 0;
+	int rv = 0;
 	char buf[BUFSIZE];
 
 	// 1) Ver si es la primera vez que se llama a "write" para este fichero
@@ -487,9 +550,12 @@ static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count,
 	if (*ppos > 0 || count > BUFSIZE) {
 		printk(KERN_ERR
 		       "/proc/fddev: Only one write allowed or too much bytes "
-		       "sent (100 bytes max)\n");
+		       "written (100 bytes max)\n");
+
 		return -EFAULT;
+
 	}
+
 	printk(KERN_DEBUG "/proc/fddev: write handler\n");
 
 	// 2) Copia "count" bytes desde memoria de espacio de user (ubuf) a
@@ -498,7 +564,9 @@ static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count,
 	// /proc/fddev:
 	if (copy_from_user(buf, ubuf, count)) {
 		printk(KERN_ERR "/proc/fddev: write handler failed\n");
+
 		return -EFAULT;
+
 	}
 
 	rv = strlen(buf);
@@ -511,13 +579,17 @@ static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count,
 	if (kstrtoint(buf, 10, &fd)) {
 		printk(KERN_ERR
 		       "/proc/fddev: can not be parsed fd from userspace\n");
+
 		return -EINVAL;
+
 	}
 
 	// 4) Comprobar que el descriptor de fichero es válido
 	if (fd < 0) {
 		printk(KERN_ERR "printH: invalid fd (%d)\n", fd);
+
 		return -EBADF;
+
 	}
 
 	// 5) Comprobar que el fd es válido en ESTE proceso
@@ -526,14 +598,15 @@ static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count,
 
 	if (!f) {
 		printk(KERN_ERR "Error printH: fget failed for fd %d\n", fd);
+
 		return -EBADF;
+
 	}
 
 	// 6) Comprobar metadatos del fichero
 	rv = val_metadata(f);
-	if (rv < 0) {
+	if (rv < 0)
 		goto out_put;
-	}
 
 	// 7) Escribir contenido del kernel certificado en fd -> HMAC(SHA-256)
 	// con clave K embebida:
@@ -541,13 +614,17 @@ static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count,
 	if (rv < 0) {
 		printk(KERN_ERR "Error mywrite: printH failed for fd %d: %d\n",
 		       fd, rv);
+
 		goto out_put;
+
 	}
+
 	printk(KERN_DEBUG "mywrite: printH OK for fd %d (%d bytes written)\n",
 	       fd, rv);
 
       out_put:
 	fput(f);
+
 	return rv;
 }
 
@@ -559,21 +636,25 @@ static ssize_t myread(struct file *file, char __user *ubuf, size_t count,
 	char buf[] = "LKM ready to receive file descriptors from user.";
 	int len = strlen(buf);
 
-	// 1) Ver si es la primera vez que se llama a "read" para este fichero
-	// --> sino EOF => single-shot
+	// 1) Ver si primera vez "read" -> sino EOF => single-shot
 	if (*ppos > 0 || count < len) {
 		printk(KERN_ERR
 		       "/proc/fddev: Only one read allowed or very few bytes "
 		       "requested\n");
+
 		return 0;
+
 	}
+
 	printk(KERN_DEBUG "/proc/fddev: read handler\n");
 
 	// 2) Copia "len" bytes desde memoria del kernel (buf) a memoria de
 	// usuario (ubuf):
 	if (copy_to_user(ubuf, buf, len)) {
 		printk(KERN_ERR "/proc/fddev: read handler failed\n");
+
 		return -EFAULT;
+
 	}
 
 	// 3) Puntero seguimiento (*ppos) del fichero en el último byte
@@ -595,31 +676,28 @@ static const struct proc_ops myops = {
 };
 
 // Cargar LKM:
-static int simple_init(void) {
+static int init(void) {
 
-	// 1) Imprime K en hexadecimal en los logs del kernel
-	// (/var/log/kern.log)
-	// -> %*phC separa bytes con ':' => SOLO EN PRUEBAS
-	printk(KERN_DEBUG "K (64Bytes) loaded = %*phC\n", K_len, K);
-
-	// 2) Crear fichero en /proc -> /proc/fddev:
+	// Crear fichero en /proc -> /proc/fddev:
 	ent = proc_create("fddev", 0660, NULL, &myops);
 	if (!ent) {
 		printk(KERN_ERR "Error creating file in /proc");
 		return -ENOMEM;
+
 	}
+
 	printk(KERN_INFO "New proc file created: /proc/fddev\n");
 
 	return 0;
 }
 
 // Descargar LKM:
-static void simple_cleanup(void) {
+static void cleanup(void) {
 
 	// 1) Borrar referencia al fichero creado en /proc -> /proc/fddev:
 	proc_remove(ent);
 	printk(KERN_INFO "Proc file deleted: /proc/fddev\n");
 }
 
-module_init(simple_init);
-module_exit(simple_cleanup);
+module_init(init);
+module_exit(cleanup);
